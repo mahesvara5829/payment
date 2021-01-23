cec2d5d67e72a86df465a8cc0706f82025cefd3b
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2021-01-23 23:23:17 +0900

    アクティブハッシュ実装完了

--- app/controllers/sp_payments_controller.rb
+++ app/controllers/sp_payments_controller.rb
@@ -9,19 +9,19 @@ class SpPaymentsController < ApplicationController
   end
 
   def create
-    @sp_payment=SpPayment.new(sp_payment_params)
+    @sp_payment=SpPayment.new(payment_params)
     if @sp_payment.save
       redirect_to root_path
     else
       render :new
-      @sp_payment=SpPayment.new(sp_payment_params)
+      @sp_payment=SpPayment.new(payment_params)
     end
 
   end
 
 
   private
-  def sp_payment_params
+  def payment_params
     params.require(:sp_payment).permit(:store_id, :kisyu_id, :investment_medal, :investment_ball, :recovery_medal, :recovery_ball, :medal, :ball, :month_id, :day_id, :memo).merge(user_id: current_user.id)
   end
 
