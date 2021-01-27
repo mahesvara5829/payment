fc86bdddc52db0edbd1c6257d6a2e04a69ad4f66
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2021-01-28 02:43:05 +0900

    非同期通信

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,30 +1,31 @@
 //function comment(){
- // const submit=document.getElementById("submit");
+  //const submit=document.getElementById("submit");
   //const posts = document.querySelectorAll(".ppp");
-//  submit.addEventListener("click",(e)=>{
-//  const postId = posts.getAttribute("data-id");
-//  const formData=new FormData(document.getElementById("form"));
-//  const XHR=new XMLHttpRequest();
-//  XHR.open("POST",`/sp_payments/${postID}/comments`,true);
-//  XHR.responseType="json";
-//  XHR.send(formData);
-//  XHR.onload=()=>{
-//    if (XHR.status!=200){
-//      alert(`Error ${XHR.status}: ${XHR.statusText}`);
-//      return null
-//    }
-//    const comment=XHR.response.post;
- //   const list=document.getEeventListener("list");
- //   const formText=document.getEventListener("content");
- //   const HTML=`
- //   <div class="ppp" data-id=<%=comment.sp_payment_id%>>
- //   <strong>${comment.user.nickname}：</strong>
- //     ${comment.text}
- //   </div>`;
- //   list.insertAdjacentHTML("afterend",HTML);
- //   formText.value="";
- // };
- // e.preventDefault();
+  //submit.addEventListener("click",(e)=>{
+  //const postId = posts.getAttribute("data-id");
+  //const formData=new FormData(document.getElementById("form"));
+  //const XHR=new XMLHttpRequest();
+  //XHR.open("POST",`/sp_payments/${postID}/comments`,true);
+  //XHR.responseType="json";
+  //XHR.send(formData);
+  //XHR.onload=()=>{
+    //if (XHR.status!=200){
+      //alert(`Error ${XHR.status}: ${XHR.statusText}`);
+      //return null
+    //}
+    //const comment=XHR.response.post;
+    //const user=XHR.response.user;
+    //const list=document.getEeventListener("list");
+    //const formText=document.getEventListener("content");
+    //const HTML=`
+    //<div class="ppp" data-id=<%=comment.sp_payment_id%>>
+    //<strong>${user.nickname}：</strong>
+      //${comment.text}
+    //</div>`;
+    //list.insertAdjacentHTML("afterend",HTML);
+    //formText.value="";
+  //};
+  //e.preventDefault();
 //});
 //}
 //window.addEventListener("load",comment)
